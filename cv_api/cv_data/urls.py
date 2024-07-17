937f32fde6158e1090fc02f694fddadc73678d93
Madjar Evan <evanmadjar@gmail.com>
2024-07-17 16:37:12 +0300

    feat: Create the remaining 2 views for CV sections

--- cv_api/cv_data/urls.py
+++ cv_api/cv_data/urls.py
@@ -3,4 +3,6 @@ from . import views
 
 urlpatterns = [
     path('personal/', views.personal_view, name='personal'),
+    path('experience/', views.experience_view, name='experience'),
+    path('education/', views.education_view, name='education'),
 ]
