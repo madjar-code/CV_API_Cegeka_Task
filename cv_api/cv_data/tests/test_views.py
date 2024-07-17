2154aa8b76e2010c7945c22d367c74af729f9675
Madjar Evan <evanmadjar@gmail.com>
2024-07-17 16:38:09 +0300

    test: Test 'education' and 'experience' views

--- cv_api/cv_data/tests/test_views.py
+++ cv_api/cv_data/tests/test_views.py
@@ -7,3 +7,13 @@ class CVViewsTestCase(TestCase):
         response = self.client.get(reverse('personal'))
         self.assertEqual(response.status_code, 200)
         self.assertIn('name', response.json())
+
+    def test_get_experience(self):
+        response = self.client.get(reverse('experience'))
+        self.assertEqual(response.status_code, 200)
+        self.assertTrue(isinstance(response.json(), list))
+
+    def test_get_education(self):
+        response = self.client.get(reverse('education'))
+        self.assertEqual(response.status_code, 200)
+        self.assertTrue(isinstance(response.json(), list))
